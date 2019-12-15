90ca0f81c4862ea5a4bbd8a7eabe30da3c3cc59b
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-15 16:19:55 -0500

    set up quiz index path

---
+++ study-buddy-backend/app/controllers/api/v1/quizzes_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::QuizzesController < ApplicationController
+
+    def index 
+        @quizzes = Quiz.all 
+        render json: @quizzes
+    end 
+end
--- study-buddy-backend/config/routes.rb
+++ study-buddy-backend/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :options
-  resources :questions
-  resources :quizzes
+  namespace :api do 
+    namespace :v1 do
+      resources :quizzes
+      resources :options
+      resources :questions
+    end 
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
