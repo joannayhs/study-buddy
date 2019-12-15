91496e912cc4b78dd20fdb7d6e46c7d04d61c621
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-15 16:48:19 -0500

    built quiz controller with CRUD paths

--- study-buddy-backend/app/controllers/api/v1/quizzes_controller.rb
+++ study-buddy-backend/app/controllers/api/v1/quizzes_controller.rb
@@ -1,7 +1,34 @@
 class Api::V1::QuizzesController < ApplicationController
 
     def index 
-        @quizzes = Quiz.all 
-        render json: @quizzes
+        quizzes = Quiz.all 
+        render json: quizzes, include:[:questions, :options]
+    end 
+
+    def show  
+        @quiz = Quiz.find(params[:id])
+        render json: @quiz, include:[:questions, :options]
+    end 
+
+    def create
+        @quiz = Quiz.create(quiz_params)
+        render json: @quiz, include:[:questions, :options]
+    end 
+
+    def update 
+        @quiz = Quiz.find(params[:id])
+        @quiz.update(quiz_params)
+        render json: @quiz
+    end
+
+    def destroy 
+        @quiz = Quiz.find(params[:id])
+        @quiz.destroy 
+    end
+
+    private 
+
+    def quiz_params 
+        params.require(:quiz).permit(:title, :subject, :questions[:options[]])
     end 
 end
--- study-buddy-backend/config/routes.rb
+++ study-buddy-backend/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
-      resources :quizzes
-      resources :options
-      resources :questions
+      resources :quizzes do 
+        resources :questions 
+      end 
+    
+      resources :questions do 
+        resources :options 
+      end 
+      
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
