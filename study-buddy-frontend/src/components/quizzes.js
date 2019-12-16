362f68d001a1756409a2d57000e898cc622f2871
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-15 19:34:44 -0500

    Added function to render subject list for quizzes

--- study-buddy-frontend/src/components/quizzes.js
+++ study-buddy-frontend/src/components/quizzes.js
@@ -2,12 +2,29 @@ class Quizzes {
     constructor(){
         this.quizzes = []
         this.adapter = new QuizAdapter()
-        // this.bindEventListeners
         this.fetchAndLoadQuizzes()
     }
 
     fetchAndLoadQuizzes() {
         return this.adapter.getQuizzes()
-        .then(quiz => console.log(quiz))
+        .then(quiz => {
+            quiz.forEach(quiz => this.quizzes.push(quiz))
+        }).then(() => this.render())
+    }
+
+    render(){
+        const quizSelectContainer = document.getElementById('quiz-select-container')
+        const quizContainer = document.getElementById('quiz-container')
+
+    }
+
+    renderSubjectList() {
+        const subjectContainer = document.getElementById('subject-container')
+
+        let subjects = []
+        this.quizzes.map(quiz => subjects.push(quiz.subject))
+        let listItem = document.createElement('p')
+        subjects.forEach(subject => listItem.innerHTML = subject)
+        subjectContainer.appendChild(listItem)
     }
 }
