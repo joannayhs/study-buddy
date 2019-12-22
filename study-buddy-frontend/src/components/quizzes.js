a5f644d6ebefc2ac08cd43cde38acb16d1fbf97c
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-22 15:32:44 -0500

    moved quiz methods to quiz class

--- study-buddy-frontend/src/components/quizzes.js
+++ study-buddy-frontend/src/components/quizzes.js
@@ -16,4 +16,27 @@ class Quizzes {
         const quizContainer = document.getElementById('quiz-container')
         this.quizzes.forEach(quiz => new Subject(quiz.subject, quiz))
         }
+
+    renderQuizSelection(subject) {
+        this.quizzes.forEach(quiz => this.createQuizCard(quiz))
+        hideQuizSelection()
+    }
+
+    hideQuizSelection() {
+
+    }
+
+
+    createQuizCard(quiz) {
+        const quizSelectContainer = document.getElementById('quiz-select-container')
+        quizSelectContainer.classList.remove('hide')
+
+        let quizCard = document.createElement('div')
+        quizCard.classList = "quiz-card"
+
+        let text = document.createElement('p')
+        text.innerText = quiz.title
+        quizCard.appendChild(text)
+        quizSelectContainer.appendChild(quizCard)
+    }
 }
--- study-buddy-frontend/src/components/subject.js
+++ study-buddy-frontend/src/components/subject.js
@@ -11,24 +11,9 @@ class Subject {
         
         let listItem = document.createElement('p')
         listItem.innerHTML = this.subject
-        listItem.addEventListener('click', (e) => this.renderQuizSelection(e.target.innerText))
+        listItem.addEventListener('click', (e) => renderQuizSelection(e.target.innerText))
         subjectContainer.appendChild(listItem) 
 
-    }
-
-   renderQuizSelection(subject) {
-    this.quizzes.forEach(quiz => this.createQuizCard(quiz))
-   }
-
-    createQuizCard(quiz) {
-        const quizSelectContainer = document.getElementById('quiz-select-container')
-
-        let quizCard = document.createElement('div')
-        quizCard.classList = "quiz-card"
-        let text = document.createElement('p')
-        text.innerText = quiz.title
-        quizCard.appendChild(text)
-        quizSelectContainer.appendChild(quizCard)
     }
 
 }
