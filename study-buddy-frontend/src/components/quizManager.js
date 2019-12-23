8f142bf4e18a9d8c70cf6493d6af588f41ffcb4c
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-22 20:21:25 -0500

    added question property to quiz manager class

--- study-buddy-frontend/src/components/quizManager.js
+++ study-buddy-frontend/src/components/quizManager.js
@@ -1,23 +1,23 @@
 class QuizManager {
     constructor(quiz){
         this.quiz = quiz
+        this.questions = quiz.questions
         this.renderQuiz()
     }
 
     renderQuiz(){
         const quizContainer = document.getElementById('quiz-container')
         quizContainer.classList.remove('hide')
+        let quizTitle = document.createElement('h1')
+        quizTitle.innerText = this.quiz.title
+        quizContainer.appendChild(quizTitle)
         this.loadQuestion()
     }
 
     loadQuestion() {
         const quizContainer = document.getElementById('quiz-container')
-
         let questionCard = document.createElement('div')
         questionCard.classList.add('question-card')
-        let quizTitle = document.createElement('h1')
-        quizTitle.innerText = this.quiz.title
-        questionCard.appendChild(quizTitle)
         quizContainer.appendChild(questionCard)
         
     }
