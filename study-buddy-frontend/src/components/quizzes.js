2a6ffd759a80f7be67c5a6cf7b56f07bc87323b4
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-25 12:39:06 -0500

    deleted subject class

--- study-buddy-frontend/src/components/quizzes.js
+++ study-buddy-frontend/src/components/quizzes.js
@@ -19,7 +19,44 @@ class Quizzes {
     }
 
     render() {
-        this.subjects.forEach(subject => new Subject(subject))
+        this.renderSubject()
+    }
+
+    renderSubject() {
+        const subjectContainer = document.getElementById('subject-container')
+
+        this.subjects.forEach(subject => {
+        let listItem = document.createElement('p')
+        listItem.innerHTML = subject
+        listItem.addEventListener('click', (e) => this.renderQuizSelection())
+        subjectContainer.appendChild(listItem)
+        })
+    }
+
+    renderQuizSelection() {
+        let quizCards = document.querySelectorAll('.quiz-card')
+        quizCards.forEach(card => card.parentElement.removeChild(card))
+        this.quizzes.forEach(quiz => this.createQuizCard(quiz))
+    }
+
+
+    createQuizCard(quiz) {
+        const quizSelectContainer = document.getElementById('quiz-select-container')
+        quizSelectContainer.classList.remove('hide')
+
+        let quizCard = document.createElement('div')
+        quizCard.classList.add('quiz-card')
+
+        let text = document.createElement('p')
+        text.innerText = quiz.title
+        quizCard.appendChild(text)
+        quizSelectContainer.appendChild(quizCard)
+        quizCard.addEventListener('click', (e) => {
+            new QuizManager(quiz)
+            quizCard.parentElement.removeChild(quizCard)
+            const subjectContainer = document.getElementById('subject-container')
+            console.log(subjectContainer)
+        })
     }
 
 }
