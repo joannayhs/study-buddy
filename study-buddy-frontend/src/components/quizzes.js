611ee88f009291fd2da925ae58b4a9e24f517815
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-25 12:29:47 -0500

    added logic to load unique subjects

--- study-buddy-frontend/src/components/quizzes.js
+++ study-buddy-frontend/src/components/quizzes.js
@@ -2,18 +2,24 @@ class Quizzes {
     constructor(){
         this.quizzes = []
         this.adapter = new QuizAdapter()
+        this.subjects = []
         this.fetchAndLoadQuizzes()
     }
 
     fetchAndLoadQuizzes() {
         return this.adapter.getQuizzes()
         .then(quiz => {
-            quiz.forEach(quiz => this.quizzes.push(quiz))
+            quiz.forEach(quiz => {
+                this.quizzes.push(quiz)
+                if(!this.subjects.includes(quiz.subject)){
+                    this.subjects.push(quiz.subject)
+                }
+            })
         }).then(() => this.render())
     }
 
     render() {
-        this.quizzes.forEach(quiz => new Subject(quiz.subject, quiz))
+        this.subjects.forEach(subject => new Subject(subject))
     }
 
 }
