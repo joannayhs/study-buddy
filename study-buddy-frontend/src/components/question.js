e9f928aaed5bf7242f1beb2f13b62a957ae11b1c
Joanna Yuhas <joannayuhas@gmail.com>
2019-12-27 19:57:49 -0500

    check for correct answers and go to next question

--- study-buddy-frontend/src/components/question.js
+++ study-buddy-frontend/src/components/question.js
@@ -2,14 +2,15 @@ class Question {
     constructor(quiz){
         this.adapter = new QuestionAdapter()
         this.quiz = quiz
+        this.quizQuestions = undefined
         this.fetchAndloadQuestion()
         this.currentQuestionIndex = 0
     }
     fetchAndloadQuestion() {
         return this.adapter.getQuestions()
             .then(questions => {
-                let quizQuestions = questions.filter(question => question.quiz_id === this.quiz.id)
-                this.renderQuestion(quizQuestions)
+                this.quizQuestions = questions.filter(question => question.quiz_id === this.quiz.id)
+                this.renderQuestion(this.quizQuestions)
             })
     }
 
@@ -18,12 +19,23 @@ class Question {
 
         let questionCard = document.createElement('div')
         questionCard.classList.add('question-card')
-        questionCard.innerText = questionArray[this.currentQuestionIndex].text
         quizContainer.appendChild(questionCard)
-        this.loadOptions(questionArray[this.currentQuestionIndex])
 
+        this.nextQuestion(questionArray[this.currentQuestionIndex])
     }
     
+    nextQuestion(question){
+        let questionCard = document.querySelector('.question-card')
+
+        questionCard.innerText = ""
+        questionCard.classList.remove('correct')
+        questionCard.classList.remove('incorrect')
+
+        questionCard.innerText = question.text
+        this.loadOptions(question)
+
+    }
+
     loadOptions(question) {
         const questionCard = document.querySelector('.question-card')
         let options = []
@@ -34,15 +46,41 @@ class Question {
             optionButton.innerText = option.text 
             questionCard.appendChild(optionButton)
             optionButton.addEventListener('click', (e) => {
-                this.currentQuestionIndex++ 
+                this.currentQuestionIndex++
+                let optionButtons = document.querySelectorAll('.option-button')
+                optionButtons.forEach(button => button.parentElement.removeChild(button)) 
                 this.checkAnswer(e, options)
             })
         })
     }
 
     checkAnswer(e, options){
-        console.log(options.find(option => option.text === e.target.innerText))
-        
-
+        let questionCard = document.querySelector('.question-card')
+        let selectedAnswer = options.find(option => option.text === e.target.innerText)
+        let correctAnswer = options.find(option => option.correct === true)
+        if(selectedAnswer === correctAnswer){
+            questionCard.classList.add('correct')
+            this.checkIfComplete()
+        }else{
+            questionCard.classList.add('incorrect')
+            this.checkIfComplete()
+        }
     }
+
+        checkIfComplete(){
+            let questionCard = document.querySelector('.question-card')
+            if (this.quizQuestions.length > this.currentQuestionIndex) {
+                let nextButton = document.createElement('button')
+                nextButton.innerText = "Next Question"
+                questionCard.appendChild(nextButton)
+                nextButton.addEventListener('click', (e) => {
+                    this.nextQuestion(this.quizQuestions[this.currentQuestionIndex])
+                })
+            } else {
+                let completeQuizButton = document.createElement('button')
+                completeQuizButton.innerText = "Complete Quiz"
+                questionCard.appendChild(completeQuizButton)
+            }
+        }
+        
 }
